ce5a5659301538b4efcc85ccfffa8f30452c5911
EbuchiTsukasa <tsukasa_ebuchi@demia.co.jp>
2024-03-04 22:37:55 +0900

    gitignore追加

--- sampleSNS/urls.py
+++ sampleSNS/urls.py
@@ -3,7 +3,9 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name='index'),
-    path("home/", views.Home.as_view, name='home'),
-    path("mypost/", views.MyPost.as_view(), name='mypost'),
-    path("follow-list/", views.FollowList.as_view(), name='follow-list'),
+    path("home/<int:pk>", views.Home.as_view, name='home'),
+    path("mypost/<int:pk>", views.MyPost.as_view(), name='mypost'),
+    path("other_user_post_list/<int:pk>", views.OtherUserPostList.as_view(), name='other_user_post_list'),
+    path("like_delete/<int:pk>", views.LikeDelete.as_view(), name='like_delete'),
+    path("like_create/<int:pk>", views.LikeCreate.as_view(), name='like_create'),
 ]
--- sampleSNS/views.py
+++ sampleSNS/views.py
@@ -1,7 +1,9 @@
-from .models import Post
-from django.shortcuts import render
+from django.db.models.query import QuerySet
+from .models import Post, Like
+from django.views import View
+from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.mixins import LoginRequiredMixin
-from django.views.generic import ListView, DetailView, CreateView
+from django.views.generic import ListView, DetailView, CreateView, DeleteView
 from django.urls import reverse_lazy
 
 def index(request):
@@ -19,12 +21,28 @@ class Home(LoginRequiredMixin, ListView):
 class MyPost(LoginRequiredMixin, ListView):
     """自分の投稿のみ表示"""
     model = Post
-    template_name = 'list.html'
+    template_name = 'mypost.html'
 
     def get_queryset(self):
         #自分の投稿に限定
         return Post.objects.filter(user=self.request.user)
 
-class FollowList(LoginRequiredMixin, ListView):
+class OtherUserPostList(LoginRequiredMixin, ListView):
     model = Post
-    template_name = 'list.html'
+    template_name = 'other_user_post_list.html'
+
+    def get_queryset(self):
+        return Post.objects.exclude(user=self.request.user)
+
+class LikeCreate(LoginRequiredMixin, View):
+    
+    def post(self, request, *args, **kwargs):
+        target = get_object_or_404(Post, pk=self.kwargs['pk'])
+        Like.objects.create(target=target, user=self.request.user)
+        return redirect('other_user_post_list', pk=self.request.user.pk)
+
+class LikeDelete(DeleteView):
+    model = Like
+
+    def get_success_url(self):
+        return redirect('other_user_post_list')
