330d51ca81ee17100b39fb41b9f4a2cfab890909
EbuchiTsukasa <tsukasa_ebuchi@demia.co.jp>
2024-03-05 15:04:30 +0900

    いいね機能

--- sampleSNS/views.py
+++ sampleSNS/views.py
@@ -1,22 +1,21 @@
 from django.db.models.query import QuerySet
 from .models import Post, Like
 from django.views import View
+from django.http import HttpResponseRedirect
 from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.views.generic import ListView, DetailView, CreateView, DeleteView
-from django.urls import reverse_lazy
+from django.urls import reverse, reverse_lazy
 
 def index(request):
     return render(request, "index.html")
 
 class Home(LoginRequiredMixin, ListView):
-    """HOMEページで、自分以外のユーザー投稿をリスト表示"""
     model = Post
-    template_name = 'list.html'
+    template_name = 'allpost.html'
 
     def get_queryset(self):
-        #リクエストユーザーのみ除外
-        return Post.objects.exclude(user=self.request.user)
+        return Post.objects.all()
    
 class MyPost(LoginRequiredMixin, ListView):
     """自分の投稿のみ表示"""
@@ -33,16 +32,27 @@ class OtherUserPostList(LoginRequiredMixin, ListView):
 
     def get_queryset(self):
         return Post.objects.exclude(user=self.request.user)
+    
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        user_likes = Like.objects.filter(user=self.request.user)
+        context['like_exists'] = {like.target.id: True for like in user_likes}
+        context['user_likes'] = {like.target.id: like for like in user_likes}
+        return context
 
 class LikeCreate(LoginRequiredMixin, View):
-    
+
     def post(self, request, *args, **kwargs):
         target = get_object_or_404(Post, pk=self.kwargs['pk'])
         Like.objects.create(target=target, user=self.request.user)
-        return redirect('other_user_post_list', pk=self.request.user.pk)
-
-class LikeDelete(DeleteView):
-    model = Like
-
-    def get_success_url(self):
         return redirect('other_user_post_list')
+
+class LikeDelete(LoginRequiredMixin, View):
+
+    def post(self, request, *args, **kwargs):
+        if request.method == 'POST':
+            like = Like.objects.filter(target_id=kwargs['pk'], user=request.user)
+        if like:
+            like.delete()
+        return redirect('other_user_post_list')
+
